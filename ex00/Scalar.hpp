df1acd5907bf3057c6760c05529b2898ac786891
thee-falcon <omar.makran.10@gmail.com>
2024-05-01 17:51:59 +0100

    Header File is done

--- ex00/Scalar.hpp
+++ ex00/Scalar.hpp
@@ -6,12 +6,36 @@
 /*   By: omakran <omakran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 17:05:36 by omakran           #+#    #+#             */
-/*   Updated: 2024/05/01 17:06:53 by omakran          ###   ########.fr       */
+/*   Updated: 2024/05/01 17:49:17 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCALAR_HPP  
 # define SCALAR_HPP
 
+# include <iostream>
+# include <string>
+
+class ScalarConverter
+{
+private:
+    /* ####################################
+    #          Orthodox Style             #
+    # ################################### */
+
+    // constructor:
+    ScalarConverter();
+    // copy constructor:
+    ScalarConverter( const ScalarConverter& other );
+    // assignement operator:
+    ScalarConverter& operator=( const ScalarConverter& other );
+public:
+
+    // static method:
+    static void convert( std::string& arg);
+
+    // destructor:
+    ~ScalarConverter();
+};
 
 #endif
