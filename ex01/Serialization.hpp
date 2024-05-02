acb61a161c5c10140b67e9f48858aa924c5e24ec
thee-falcon <omar.makran.10@gmail.com>
2024-05-02 22:30:43 +0100

    the HeaderFiles is done

--- ex01/Serialization.hpp
+++ ex01/Serialization.hpp
@@ -6,7 +6,38 @@
 /*   By: omakran <omakran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 22:11:15 by omakran           #+#    #+#             */
-/*   Updated: 2024/05/02 22:11:16 by omakran          ###   ########.fr       */
+/*   Updated: 2024/05/02 22:26:21 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SERIALIZATION_HPP
+# define SERIALIZATION_HPP
+
+# include <cstdint>
+# include <iostream>
+# include <Data.hpp>
+
+
+class Serialization
+{
+    /* ####################################
+    #          Orthodox Style             #
+    # ################################### */
+
+    // constructor:
+    Serialization();
+    // copy constructor:
+    Serialization( const Serialization& other );
+    // assignement operator:
+    Serialization& operator=( const Serialization& other );
+    // destructor:
+    ~Serialization();
+
+public:
+    static uintptr_t    serialize(Data* ptr);
+    static Data*        deserialize(uintptr_t raw);
+
+};
+
+
+#endif
