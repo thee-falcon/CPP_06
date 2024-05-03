7ac6af61192dc41361c988bb45d8a3be5ae6f2d9
thee-falcon <omar.makran.10@gmail.com>
2024-05-03 22:19:05 +0100

    all is good, ex02 is done alhamdo-lilah

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,18 @@
 /*   By: omakran <omakran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 23:43:09 by omakran           #+#    #+#             */
-/*   Updated: 2024/05/02 23:43:10 by omakran          ###   ########.fr       */
+/*   Updated: 2024/05/03 22:07:11 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Identify.hpp"
+
+int main()
+{
+    Base *ptr = generate();  // Generate a random Base pointer
+    identify(*ptr);          // Identify the dynamic type of the object pointed to by ptr
+    identify(ptr);           // Passes the pointer itself, not the object it points to
+    delete ptr;              // Deallocate the dynamically allocated memory
+
+    return 0;
+}
