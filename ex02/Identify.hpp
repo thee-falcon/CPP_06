68136aacbf6d72f4b501b3de884cae855e8ed08c
thee-falcon <omar.makran.10@gmail.com>
2024-05-03 21:33:28 +0100

    The Header File is done

--- ex02/Identify.hpp
+++ ex02/Identify.hpp
@@ -6,7 +6,32 @@
 /*   By: omakran <omakran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 23:43:06 by omakran           #+#    #+#             */
-/*   Updated: 2024/05/02 23:43:07 by omakran          ###   ########.fr       */
+/*   Updated: 2024/05/03 17:45:08 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef IDENTIFY_HPP
+# define IDENTIFY_HPP
+
+# include <cstdlib>
+# include <iostream>
+
+/* ################################################################
+    These four classes don’t have to be designed in the Orthodox
+                    Canonical Form.
+* ################################################################# */ 
+class Base
+{
+public:
+    virtual ~Base();
+};
+
+class A: public Base {};
+class B: public Base {};
+class C: public Base {};
+
+Base*   generate(void);
+void    identify(Base* p);
+void    identify(Base& p);
+
+#endif
